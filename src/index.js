713c6f4d04557acae20b8c176c147a5368842363
rostislavtsonev <rtsonev@nemetschek.bg>
2019-04-15 17:29:08 +0300

    Add lines needed to use Redux Dev Tool.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, compose} from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers());
 
 ReactDOM.render(
   <Provider store={store}>
